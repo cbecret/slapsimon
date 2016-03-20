482c3bdc970dca25ec0c1549567c608a0aeaf1e0
Cyril Becret <cyril.becret@gmail.com>
2016-03-20 21:58:31 +0100

    Prototype 2.1

--- js/easelJsUtils.js
+++ js/easelJsUtils.js
@@ -1,42 +1,69 @@
-(function(){
+(function() {
 
 	var Graphics = createjs.Graphics;
 	var Shape = createjs.Shape;
 
 	// Constructeur EaselJsUtils
-    EaselJsUtils = function(stage) {
-    	this.stage = stage;
-    };
+	EaselJsUtils = function(stage) {
+		this.stage = stage;
+	};
 
 
-     // Classe EaselJsUtils
-    EaselJsUtils.prototype = {
+	// Classe EaselJsUtils
+	EaselJsUtils.prototype = {
 
 		// Fonction de gestion d'images
 		createBitmap: function(src, x, y, options) {
-		    // Définir la source et la position du média
-		    var bitmap = new createjs.Bitmap(src);
-		    bitmap.x = x;
-		    bitmap.y = y;
-		    // Appliquer les options
-		    if (options) {
-		        // Mise à l'échelle
-		        if (options.scale) {
-		            bitmap.scaleX = options.scale[0];
-		            bitmap.scaleY = options.scale[1];
-		        }
-		    }
-		    // Ajouter le Bitmap au Stage et le retourner
-		    this.stage.addChild(bitmap);
-		    return bitmap;
+			// Définir la source et la position du média
+			var bitmap = new createjs.Bitmap(src);
+			bitmap.x = x;
+			bitmap.y = y;
+			// Appliquer les options
+			if (options) {
+				// Mise à l'échelle
+				if (options.scale) {
+					bitmap.scaleX = options.scale[0];
+					bitmap.scaleY = options.scale[1];
+				}
+			}
+			// Ajouter le Bitmap au Stage et le retourner
+			this.stage.addChild(bitmap);
+			return bitmap;
 		},
 
 
 		// Création d'une tête de Simon
 		createSimon: function(x, y, options) {
-		    return this.createBitmap("img/simon.png", x, y, options);
+			return this.createBitmap("./img/simon.png", x, y, options);
 		},
 
+		// Création d'un décor de licornes
+		unicornWorld: function(x, y, options) {
+			return this.createBitmap("./img/unicorn.png", x, y, options);
+		},
+
+		// Création d'un corps de licorne
+		unicornBody: function(x, y, options) {
+			return this.createBitmap("./img/une_licorne.png", x, y, options);
+		},
+
+		// 
+		showForce: function(u, v, x, y, force) {
+
+			return this.createBitmap("./, ")
+		},
+
+
+		pythagore: function(x, y) {
+			return Math.sqrt((x * x) + (y * y));
+		},
+
+		calcDeplacement: function(lastX, lastY, mouseX, mouseY) {
+			var slapX = lastX - mouseX;
+			var slapY = lastY - mouseY;
+			var deplacement = this.pythagore(slapX, slapY);
+			return deplacement;
+		},
 
 		// Création d'un titre (ou texte si besoin)
 		createTitre: function(text, font, x, y, options) {
@@ -47,24 +74,23 @@
 			titre.x = x;
 			titre.y = y;
 			// Appliquer les options
-		    if (options) {
-		        if (options.color) {
-		            titre.color = options.color;
-		        }
-		        if (options.textAlign) {
-		            titre.textAlign = options.textAlign;
-		        }
-		        if (options.cursor) {
-		            titre.cursor = options.cursor;
-		        }
-		    }
-		    // Ajouter le Text au Stage et le retourner
-		    this.stage.addChild(titre);
-		    return titre;
+			if (options) {
+				if (options.color) {
+					titre.color = options.color;
+				}
+				if (options.textAlign) {
+					titre.textAlign = options.textAlign;
+				}
+				if (options.cursor) {
+					titre.cursor = options.cursor;
+				}
+			}
+			// Ajouter le Tesxt au Stage et le retourner
+			this.stage.addChild(titre);
+			return titre;
 		}
 
 
 
-
-    };
+	};
 }());
--- js/gip.js
+++ js/gip.js
@@ -1,65 +1,90 @@
-(function(){
+(function() {
 
 	var simon = {
-		simonX: 300,
-		simonY: 150
+		simonX: 440,
+		simonY: 230
 	};
 
 
 	// Initialisation du jeu
 	$(document).ready(function() {
-	    init();
+		init();
 	});
-	 
+
 
 	this.init = function() {
-	    // Initialisation du niveau
-	    prepareStage();
-	    addTitre();
-	    addBitmaps(); // ajoute les images
-	    startTicker(30); // défini les fps
+		// Initialisation du niveau
+		prepareStage();
+		addTitre();
+		addBitmaps(); // ajoute les images
+		startTicker(30); // défini les fps
 	};
+
 
 
 	// Préparer le stage et instancie EaselJsUtils
 	this.prepareStage = function() {
-	    this.canvas = $('#Canvas').get(0);
-	    this.stage = new createjs.Stage(this.canvas);
-	    this.stage.on("stagemousedown", function(evt) {
-	    	var clickX = Math.round(evt.stageX);
-	    	var clickY = Math.round(evt.stageY);
-	    	simon.simonX += 10;
-	    	if ( simon.simonX && simon.simonY) {
-	    		simon.simonX = clickX - 200;
-	    		simon.simonY = clickY - 200;
-	    	}
-	    	addBitmaps();
-	    	console.log("SimonXXxxXxxx : " + simon.simonX);
-    		console.log("the canvas was clicked at "+clickX+","+clickY);
+		this.canvas = $('#Canvas').get(0);
+		this.stage = new createjs.Stage(this.canvas);
+
+		this.stage.on("stagemousedown", function(evt) {
+			this.stage.downX = evt.stageX;
+			// Récupérer la position du curseur
+			this.stage.downY = evt.stageY;
 		});
-	    this.stage.enableMouseOver(30); // Activer la gestion des évenements souris
-	    easelJsUtils = new EaselJsUtils(this.stage);
+
+		this.stage.on("stagemouseup", function(evt) {
+
+			// Récupérer la position du curseur
+			this.stage.upX = evt.stageX;
+			this.stage.upY = evt.stageY;
+
+			console.log("CLICK DOWN en X : " + this.stage.downX + ", " + " , CLICK DOWN en Y : " + this.stage.downY);
+			console.log("CLICK UP en X : " + this.stage.upX + " , CLICK UP en Y : " + this.stage.upY);
+			var force = easelJsUtils.calcDeplacement(this.stage.upX, this.stage.upY, this.stage.downX, this.stage.downY);
+			console.log("Voilà la foooooorce : " + force);
+			console.log("Verification de la force :" + easelJsUtils.pythagore(900, 1200));
+		});
+
+
+		this.stage.enableMouseOver(30); // Activer la gestion des évenements souris
+		easelJsUtils = new EaselJsUtils(this.stage);
 	};
 
 
 	this.addTitre = function() {
-		var titre = easelJsUtils.createTitre("Slap Simon !!", "100px Impact", 600, 20, {color: '#FF2020', textAlign: 'center'});
+		var titre = easelJsUtils.createTitre("Slap Simon !!", "100px Impact", 600, 20, {
+			color: '#FF2020',
+			textAlign: 'center'
+		});
 	};
 
 
 	// Ajout des images	     
 	this.addBitmaps = function() {
-	    // Créé la tête de Simon
-	    var simonHead = easelJsUtils.createSimon(simon.simonX, simon.simonY, {scale:[1, 1]});
+		// Créer le monde des licornes
+		var unicornWorld = easelJsUtils.unicornWorld(0, 0, {
+			scale: [0.8, 0.9]
+		});
+
+		var uniBody = easelJsUtils.unicornBody(450, 50, {
+			scale: [2, 2]
+		});
+
+		// Créé la tête de Simon
+		var simonHead = easelJsUtils.createSimon(simon.simonX, simon.simonY, {
+			scale: [0.5, 0.5]
+		});
+
 	};
 
 
 	// Permet de rafraichir le rendu en fonction du fps défini
 	this.startTicker = function(fps) {
-	    createjs.Ticker.setFPS(fps);
-	    createjs.Ticker.addEventListener("tick", function(){
-	        this.stage.update();
-	    });
+		createjs.Ticker.setFPS(fps);
+		createjs.Ticker.addEventListener("tick", function() {
+			this.stage.update();
+		});
 	};
 
 }());
